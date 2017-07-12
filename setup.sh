572b4e1e871f80c5e9cb08d8205d7c661a79c724
rvsmantri <rvsmantri@users.noreply.github.com>
2017-07-12 14:34:06 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
+#get gcloud utilities
+curl https://sdk.cloud.google.com | bash
+#restart shell and run
+exec -l $SHELL
+#Initialize gcloud
+gcloud init
 #create dataproc-datalab cluster on gcp
 gcloud dataproc clusters create cluster-1 --zone us-west1-a --master-machine-type n1-standard-4 --master-boot-disk-size 500 --num-workers 2 --worker-machine-type n1-standard-4 --worker-boot-disk-size 500 --image-version preview --scopes 'https://www.googleapis.com/auth/cloud-platform' --project polaris-dev --initialization-actions 'gs://dataproc-datalab-setup/datalab.sh' --initialization-actions gs://datalab-dataproc/datalab.sh  --scopes cloud-platform
 
